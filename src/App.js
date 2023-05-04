0bed3f2d3e6270991b61999dcba80da547c2af24
prasanth <prasanthsankar32@gmail.com>
2023-05-04 14:23:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
+
+const initialState = { count : 0 };
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [state,dispatch] = useReducer(reducer,initialState); 
+
+  return(
+    <div>
+      Count : { state.count }
+      {/* <button onClick={() => dispatch({type:'increment'})}>+</button>
+      <button onClick={() => dispatch({type:'decrement'})}>-</button> */}
+      <button onClick={() => dispatch({type:'increment',payload:5})}>+</button>
+      <button onClick={() => dispatch({type:'decrement',payload:5})}>-</button>
     </div>
-  );
+  )
 }
 
 export default App;
+
+function reducer(state,action) {
+  switch(action.type) {
+    case 'increment':
+      return {count : state.count + action.payload}
+    case 'decrement':
+      return {count : state.count - action.payload}
+    default:
+      throw new Error();
+  }
+}
